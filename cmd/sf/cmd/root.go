7c32fe375bcb2a3768fc209b6d93be9758c86ed5
Stéphane Duchesneau <stephane.duchesneau@streamingfast.io>
2022-02-23 14:37:32 -0500

    fix broken usage examples (removed filters)

--- cmd/sf/cmd/root.go
+++ cmd/sf/cmd/root.go
@@ -19,9 +19,6 @@ If STREAMINGFAST_API_KEY environment is not set, only unauthenticated network
 will be connectable to, authenticated network will refuse the connection.
 
 Parameters:
-  <filter>        Optional A valid CEL filter expression for the Ethereum network, only
-                  transactions matching the filter will be returned to you.
-
   <start_block>   Optional block number where to start streaming blocks from,
                   Can be positive (an absolute reference to a block), or
                   negative (a number of blocks from the tip of the chain).
@@ -30,20 +27,23 @@ Parameters:
 				  the stream of blocks will stop If not specified, the stream
 				  will stop when the Ethereum network stops: never.
 Examples:
-  # Watch all calls to the UniswapV2 Router, for a single block and close
-  $ sf eth "to in ['0x7a250d5630b4cf539739df2c5dacb4c659f2488d']" 11700000 11700001
 
-  # Watch all calls to the UniswapV2 Router, include the last 100 blocks, and stream forever
-  $ sf eth "to in ['0x7a250d5630b4cf539739df2c5dacb4c659f2488d']" -100
+
+  # Look at ALL blocks in a given range on ETH mainnet
+  $ sf eth 100000 100010
+
+  # Stream blocks in a given range on BSC with logs that match a given address and event signature (topic0) 
+  #   (transactions that do not match are filtered out of the "transactionTraces" array in the response)
+  # sf eth --bsc --log-filter-addresses='0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73' --log-filter-event-sigs='0x0d3648bd0f6ba80134a33ba9275ac585d9d315f0ad8355cddefde31afa28d0e9' 6500000 6810800
 
   # Continue where you left off, start from the last known cursor, get all fork notifications (UNDO, IRREVERSIBLE), stream forever
   $ sf eth --handle-forks --start-cursor "10928019832019283019283" "to in ['0x7a250d5630b4cf539739df2c5dacb4c659f2488d']"
 
-  # Look at ALL blocks in a given range on Binance Smart Chain (BSC)
-  $ sf eth --bsc "true" 100000 100002
+  # Stream blocks from the last 5 on NEAR testnet
+  $ sf near --testnet -- -5
 
   # Look at ALL blocks in a given range on Polygon Chain
-  $ sf eth --polygon "true" 100000 100002
+  $ sf eth --polygon 100000 100010
 `
 
 // RootCmd represents the eosc command
